7639efccf31d21b3a2459096db92fd4cb003d5b8
dtcookie <come.to.the.dark.side.cookie@gmail.com>
2018-08-08 10:19:26 +0200

    documentation

--- libdtapi/api_anomaly_detection.go
+++ libdtapi/api_anomaly_detection.go
@@ -5,262 +5,219 @@ import (
 	dtapi "github.com/dtcookie/dtapi/libdtapiconf"
 )
 
-// AnomalyDetectionAPI TODO: documentation
+// AnomalyDetectionAPI is a cleaned up wrapper around the
+// Anomaly Detection functinality offered via
+// "github.com/dtcookie/dtapi/libdtapiconf"
 type AnomalyDetectionAPI confService
 
-// AwsAnomalyDetectionAPI TODO: documentation
+// AwsAnomalyDetectionAPI covers anomaly detection
+// for AWS
 type AwsAnomalyDetectionAPI confService
 
-// DiskAnomalyDetectionAPI TODO: documentation
+// DiskAnomalyDetectionAPI covers anomaly detection
+// for Disks
 type DiskAnomalyDetectionAPI confService
 
-// HostAnomalyDetectionAPI TODO: documentation
+// HostAnomalyDetectionAPI covers anomaly detection
+// for Hosts
 type HostAnomalyDetectionAPI confService
 
-// ServiceAnomalyDetectionAPI TODO: documentation
+// ServiceAnomalyDetectionAPI covers anomaly detection
+// for Services
 type ServiceAnomalyDetectionAPI confService
 
-// VMwareAnomalyDetectionAPI TODO: documentation
+// VMwareAnomalyDetectionAPI covers anomaly detection for
+// VMWare
 type VMwareAnomalyDetectionAPI confService
 
-/*
-AWS TODO: document
-*/
+// AWS returns an API covering Anomaly Detection for AWS
 func (api AnomalyDetectionAPI) AWS() AwsAnomalyDetectionAPI {
 	return AwsAnomalyDetectionAPI{
 		client: api.client,
 	}
 }
 
-/*
-VMware TODO: document
-*/
+// VMware returns an API covering Anomaly Detection for VMware
 func (api AnomalyDetectionAPI) VMware() VMwareAnomalyDetectionAPI {
 	return VMwareAnomalyDetectionAPI{
 		client: api.client,
 	}
 }
 
-/*
-Disks TODO: document
-*/
+// Disks returns an API covering Anomaly Detection for Disks
 func (api AnomalyDetectionAPI) Disks() DiskAnomalyDetectionAPI {
 	return DiskAnomalyDetectionAPI{
 		client: api.client,
 	}
 }
 
-/*
-Hosts TODO: document
-*/
+// Hosts returns an API covering Anomaly Detection for Hosts
 func (api AnomalyDetectionAPI) Hosts() HostAnomalyDetectionAPI {
 	return HostAnomalyDetectionAPI{
 		client: api.client,
 	}
 }
 
-/*
-Services TODO: document
-*/
+// Services returns an API covering Anomaly Detection for Services
 func (api AnomalyDetectionAPI) Services() ServiceAnomalyDetectionAPI {
 	return ServiceAnomalyDetectionAPI{
 		client: api.client,
 	}
 }
 
-/*
-GET Get configuration of anomaly detection for AWS.
-@return AwsAnomalyDetectionConfig
-*/
-func (api AwsAnomalyDetectionAPI) GET() (dtapi.AwsAnomalyDetectionConfig, error) {
+// Get retrieves the current settings for Anomaly Detection related to AWS
+func (api AwsAnomalyDetectionAPI) Get() (dtapi.AwsAnomalyDetectionConfig, error) {
 	result, _, err := api.client.AnomalyDetectionAWSApi.GetAwsAnomalyDetectionConfig1(nil)
 	return result, err
 }
 
-/*
-UPDATE Updates (HTTP PUT) configuration of anomaly detection for AWS.
- * @param optional nil or *UpdateAwsAnomalyDetectionConfig1Opts - Optional Parameters:
- * @param "AwsAnomalyDetectionConfig" (optional.Interface of AwsAnomalyDetectionConfig) -
-*/
-func (api AwsAnomalyDetectionAPI) UPDATE(c dtapi.AwsAnomalyDetectionConfig) error {
+// Update updates (HTTP PUT) configuration of anomaly detection for AWS.
+func (api AwsAnomalyDetectionAPI) Update(c dtapi.AwsAnomalyDetectionConfig) error {
 	_, err := api.client.AnomalyDetectionAWSApi.UpdateAwsAnomalyDetectionConfig1(nil, &dtapi.UpdateAwsAnomalyDetectionConfig1Opts{
 		AwsAnomalyDetectionConfig: optional.NewInterface(c),
 	})
 	return err
 }
 
-/*
-VALIDATE Validate configuration of anomaly detection for AWS (for PUT request).
- * @param optional nil or *ValidateAwsAnomalyDetectionConfig1Opts - Optional Parameters:
- * @param "AwsAnomalyDetectionConfig" (optional.Interface of AwsAnomalyDetectionConfig) -
-*/
-func (api AwsAnomalyDetectionAPI) VALIDATE(c dtapi.AwsAnomalyDetectionConfig) error {
+// Validate allows for validating the configuration for Anomaly Detection related to AWS
+// before the actual Update.
+func (api AwsAnomalyDetectionAPI) Validate(c dtapi.AwsAnomalyDetectionConfig) error {
 	_, err := api.client.AnomalyDetectionAWSApi.ValidateAwsAnomalyDetectionConfig1(nil, &dtapi.ValidateAwsAnomalyDetectionConfig1Opts{
 		AwsAnomalyDetectionConfig: optional.NewInterface(c),
 	})
 	return err
 }
 
-/*
-CREATE Creates (HTTP POST) a new disk event rule.
- * @param optional nil or *CreateDiskEventConfig1Opts - Optional Parameters:
- * @param "DiskEventAnomalyDetectionConfig" (optional.Interface of DiskEventAnomalyDetectionConfig) -
-@return DiskEventAnomalyDetectionConfig
-*/
-func (api DiskAnomalyDetectionAPI) CREATE(c dtapi.DiskEventAnomalyDetectionConfig) (dtapi.DiskEventAnomalyDetectionConfigStub, error) {
+// Create creates (HTTP POST) a new rule for Disk Event Anomaly Detection
+// Returns:
+// 	- DiskEventAnomalyDetectionConfigStub	 a name/ID pair for the created disk event rule
+func (api DiskAnomalyDetectionAPI) Create(c dtapi.DiskEventAnomalyDetectionConfig) (dtapi.DiskEventAnomalyDetectionConfigStub, error) {
 	result, _, err := api.client.AnomalyDetectionDiskEventsApi.CreateDiskEventConfig1(nil, &dtapi.CreateDiskEventConfig1Opts{
 		DiskEventAnomalyDetectionConfig: optional.NewInterface(c),
 	})
 	return result, err
 }
 
-/*
-DELETE Deletes the disk alert rule with the given id.
- * @param id
-*/
-func (api DiskAnomalyDetectionAPI) DELETE(ID string) error {
+// Delete removes a rule for Disk Event Anomaly Detection
+//
+// Parameters:
+//	- ID	the unique identifier of the rule to remove
+func (api DiskAnomalyDetectionAPI) Delete(ID string) error {
 	_, err := api.client.AnomalyDetectionDiskEventsApi.DeleteDiskEventConfig1(nil, ID)
 	return err
 }
 
-/*
-GET Gets the disk event rule with the given id.
- * @param id
-@return DiskEventAnomalyDetectionConfig
-*/
-func (api DiskAnomalyDetectionAPI) GET(ID string) (dtapi.DiskEventAnomalyDetectionConfig, error) {
+// Get retrieves the detailed configuration for a Disk Event Rule
+// for Anomaly Detection
+//
+// Parameters:
+//	- ID	the unique identifier of the disk event rule
+// Returns:
+//	- DiskEventAnomalyDetectionConfig	detailed configuration of the
+//										disk event rule
+func (api DiskAnomalyDetectionAPI) Get(ID string) (dtapi.DiskEventAnomalyDetectionConfig, error) {
 	result, _, err := api.client.AnomalyDetectionDiskEventsApi.GetDiskEventConfig1(nil, ID)
 	return result, err
 }
 
-/*
-GETALL Get the list of disk event rules.
-@return DiskEventAnomalyDetectionConfigList
-*/
-func (api DiskAnomalyDetectionAPI) GETALL() ([]dtapi.DiskEventAnomalyDetectionConfigStub, error) {
+// GetAll retrieves name/ID pairs for all currently configured Disk Event Rules
+// for Anomaly Detection
+func (api DiskAnomalyDetectionAPI) GetAll() ([]dtapi.DiskEventAnomalyDetectionConfigStub, error) {
 	result, _, err := api.client.AnomalyDetectionDiskEventsApi.ListDiskEventConfigs1(nil)
 	return result.Values, err
 }
 
-/*
-UPDATE Updates (HTTP PUT) the disk event rule with the given id.
- * @param id
- * @param optional nil or *UpdateDiskEventConfig1Opts - Optional Parameters:
- * @param "DiskEventAnomalyDetectionConfig" (optional.Interface of DiskEventAnomalyDetectionConfig) -
-*/
-func (api DiskAnomalyDetectionAPI) UPDATE(id string, c dtapi.DiskEventAnomalyDetectionConfigStub) error {
-	_, err := api.client.AnomalyDetectionDiskEventsApi.UpdateOrCreateDiskEventConfig1(nil, id, &dtapi.UpdateOrCreateDiskEventConfig1Opts{
+// Update updates (HTTP PUT) a Disk Event Rule for Anomaly Detection
+//
+// Note:
+//	In case the specified DiskEventAnomalyDetectionConfig contains an
+//	identifier for the Disk Event Rule, it MUST equal the specified 'ID'
+//
+// TODO:
+//	Perform check for equality of IDs offline
+//
+// Parameters:
+//	- ID	is the unique identifier of the Disk Event Rule to update
+//	- c		is the detailed configuration of the Disk Event Rule to update.
+func (api DiskAnomalyDetectionAPI) Update(ID string, c dtapi.DiskEventAnomalyDetectionConfig) error {
+	_, err := api.client.AnomalyDetectionDiskEventsApi.UpdateOrCreateDiskEventConfig1(nil, ID, &dtapi.UpdateOrCreateDiskEventConfig1Opts{
 		DiskEventAnomalyDetectionConfig: optional.NewInterface(c),
 	})
 	return err
 }
 
-/*
-VALIDATE Validates configuration of disk event rule.
- * @param optional nil or *ValidateDiskEventConfig1Opts - Optional Parameters:
- * @param "DiskEventAnomalyDetectionConfig" (optional.Interface of DiskEventAnomalyDetectionConfig) -
-*/
-func (api DiskAnomalyDetectionAPI) VALIDATE(c dtapi.DiskEventAnomalyDetectionConfig) error {
+// Validate allows for validating the given DiskEventAnomalyDetectionConfig before performing the
+// actual Update or Create operation.
+func (api DiskAnomalyDetectionAPI) Validate(c dtapi.DiskEventAnomalyDetectionConfig) error {
 	_, err := api.client.AnomalyDetectionDiskEventsApi.ValidateDiskEventConfig1(nil, &dtapi.ValidateDiskEventConfig1Opts{
 		DiskEventAnomalyDetectionConfig: optional.NewInterface(c),
 	})
 	return err
 }
 
-/*
-GET Gets configuration of anomaly detection for hosts.
- * @param  - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-@return HostsAnomalyDetectionConfig
-*/
-func (api HostAnomalyDetectionAPI) GET() (dtapi.HostsAnomalyDetectionConfig, error) {
+// Get retrieves the detailed configuration for Host Anomaly Detection
+func (api HostAnomalyDetectionAPI) Get() (dtapi.HostsAnomalyDetectionConfig, error) {
 	result, _, err := api.client.AnomalyDetectionHostsApi.GetHostEventsConfig1(nil)
 	return result, err
 }
 
-/*
-UPDATE Updates configuration of anomaly detection for hosts.
- * @param optional nil or *UpdateHostEventsConfig1Opts - Optional Parameters:
- * @param "HostsAnomalyDetectionConfig" (optional.Interface of HostsAnomalyDetectionConfig) -
-*/
-func (api HostAnomalyDetectionAPI) UPDATE(c dtapi.HostsAnomalyDetectionConfig) error {
+// Update updates (HTTP PUT) the configuration for Host Anomaly Detection
+func (api HostAnomalyDetectionAPI) Update(c dtapi.HostsAnomalyDetectionConfig) error {
 	_, err := api.client.AnomalyDetectionHostsApi.UpdateHostEventsConfig1(nil, &dtapi.UpdateHostEventsConfig1Opts{
 		HostsAnomalyDetectionConfig: optional.NewInterface(c),
 	})
 	return err
 }
 
-/*
-VALIDATE Validates configuration of anomaly detection for hosts (for PUT request).
- * @param optional nil or *ValidateHostEventsConfig1Opts - Optional Parameters:
- * @param "HostsAnomalyDetectionConfig" (optional.Interface of HostsAnomalyDetectionConfig) -
-*/
-func (api HostAnomalyDetectionAPI) VALIDATE(c dtapi.HostsAnomalyDetectionConfig) error {
+// Validate allows for validating a configuration for Host Anomaly Detection
+// before performing the actual Update.
+func (api HostAnomalyDetectionAPI) Validate(c dtapi.HostsAnomalyDetectionConfig) error {
 	_, err := api.client.AnomalyDetectionHostsApi.ValidateHostEventsConfig1(nil, &dtapi.ValidateHostEventsConfig1Opts{
 		HostsAnomalyDetectionConfig: optional.NewInterface(c),
 	})
 	return err
 }
 
-/*
-GET Gets the service anomaly detection configuration
-@return ServiceAnomalyDetectionConfig
-*/
-func (api ServiceAnomalyDetectionAPI) GET() (dtapi.ServiceAnomalyDetectionConfig, error) {
+// Get retrieves the detailed configuration for Service Anomaly Detection
+func (api ServiceAnomalyDetectionAPI) Get() (dtapi.ServiceAnomalyDetectionConfig, error) {
 	result, _, err := api.client.AnomalyDetectionServicesApi.GetConfiguration1(nil)
 	return result, err
 }
 
-/*
-UPDATE Updates (HTTP PUT) the service anomaly detection configuration
- * @param optional nil or *UpdateConfiguration1Opts - Optional Parameters:
- * @param "ServiceAnomalyDetectionConfig" (optional.Interface of ServiceAnomalyDetectionConfig) -
-@return ServiceAnomalyDetectionConfig
-*/
-func (api ServiceAnomalyDetectionAPI) UPDATE(c dtapi.ServiceAnomalyDetectionConfig) error {
+// Update updates (HTTP PUT) the configuration for Service Anomaly Detection
+func (api ServiceAnomalyDetectionAPI) Update(c dtapi.ServiceAnomalyDetectionConfig) error {
 	_, err := api.client.AnomalyDetectionServicesApi.UpdateConfiguration1(nil, &dtapi.UpdateConfiguration1Opts{
 		ServiceAnomalyDetectionConfig: optional.NewInterface(c),
 	})
 	return err
 }
 
-/*
-VALIDATE Validates the service anomaly detection configuration
- * @param optional nil or *ValidateConfiguration1Opts - Optional Parameters:
- * @param "ServiceAnomalyDetectionConfig" (optional.Interface of ServiceAnomalyDetectionConfig) -
-*/
-func (api ServiceAnomalyDetectionAPI) VALIDATE(c dtapi.ServiceAnomalyDetectionConfig) error {
+// Validate allows for validating a configuration for Service Anomaly Detection
+// before performing the actual Update.
+func (api ServiceAnomalyDetectionAPI) Validate(c dtapi.ServiceAnomalyDetectionConfig) error {
 	_, err := api.client.AnomalyDetectionServicesApi.ValidateConfiguration1(nil, &dtapi.ValidateConfiguration1Opts{
 		ServiceAnomalyDetectionConfig: optional.NewInterface(c),
 	})
 	return err
 }
 
-/*
-GET Gets configuration of anomaly detection for VMware.
-@return VMwareAnomalyDetectionConfig
-*/
-func (api VMwareAnomalyDetectionAPI) GET() (dtapi.VMwareAnomalyDetectionConfig, error) {
+// Get retrieves the detailed configuration for VMWare Anomaly Detection
+func (api VMwareAnomalyDetectionAPI) Get() (dtapi.VMwareAnomalyDetectionConfig, error) {
 	result, _, err := api.client.AnomalyDetectionVMwareApi.GetVMwareAnomalyDetectionConfig1(nil)
 	return result, err
 }
 
-/*
-UPDATE Updates (HTTP PUT) configuration of anomaly detection for VMware.
- * @param optional nil or *UpdateVMwareAnomalyDetectionConfig1Opts - Optional Parameters:
- * @param "VMwareAnomalyDetectionConfig" (optional.Interface of VMwareAnomalyDetectionConfig) -
-*/
-func (api VMwareAnomalyDetectionAPI) UPDATE(c dtapi.VMwareAnomalyDetectionConfig) error {
+// Update updates (HTTP PUT) the configuration for VMWare Anomaly Detection
+func (api VMwareAnomalyDetectionAPI) Update(c dtapi.VMwareAnomalyDetectionConfig) error {
 	_, err := api.client.AnomalyDetectionVMwareApi.UpdateVMwareAnomalyDetectionConfig1(nil, &dtapi.UpdateVMwareAnomalyDetectionConfig1Opts{
 		VMwareAnomalyDetectionConfig: optional.NewInterface(c),
 	})
 	return err
 }
 
-/*
-VALIDATE Validates configuration of anomaly detection for VMware (for PUT request).
- * @param c config.VMwareAnomalyDetectionConfig
-*/
-func (api VMwareAnomalyDetectionAPI) VALIDATE(c dtapi.VMwareAnomalyDetectionConfig) error {
+// Validate allows for validating a configuration for VMWare Anomaly Detection
+// before performing the actual Update.
+func (api VMwareAnomalyDetectionAPI) Validate(c dtapi.VMwareAnomalyDetectionConfig) error {
 	_, err := api.client.AnomalyDetectionVMwareApi.ValidateVMwareAnomalyDetectionConfig1(nil, &dtapi.ValidateVMwareAnomalyDetectionConfig1Opts{
 		VMwareAnomalyDetectionConfig: optional.NewInterface(c),
 	})
--- libdtapi/api_anonymization.go
+++ libdtapi/api_anonymization.go
@@ -5,44 +5,71 @@ import (
 	dtapi "github.com/dtcookie/dtapi/libdtapienv"
 )
 
-// AnonymizationAPI TODO: documentation
+// AnonymizationAPI is a cleaned up wrapper around the
+// Anonymization Services offered via
+// "github.com/dtcookie/dtapi/libdtapienv"
 type AnonymizationAPI struct {
 	client    *dtapi.APIClient
 	optionals dtapi.AnonymizeOpts
 }
 
-// WithIPs TODO: documentation
+// Create Creates a User Session Anonymization Job
+//
+// Note:
+//	You can parameterize that Anonymization Job before
+//	calling this function using the build functions
+//	'WithStartTimeStamp', 'WithEndTimeStamp', 'WithIPs' and 'WithUserIDs'
+//
+// Returns:
+//	- string	the 'requestID' for the created Anonymization Job
+//				to be used to check its status later on
+func (api *AnonymizationAPI) Create() (string, error) {
+	result, _, err := api.client.AnonymizationApi.Anonymize(nil, &api.optionals)
+	api.optionals = dtapi.AnonymizeOpts{}
+	return result.RequestId, err
+}
+
+// Status Shows the progress of the Anonymization Job identified with
+// the specified 'requestID'.
+//
+// Returns:
+//	- int32	The progress of the Anonymization Job in percents.
+//			-1 if the job is waiting for execution
+func (api *AnonymizationAPI) Status(requestID string) (int32, error) {
+	result, _, err := api.client.AnonymizationApi.GetStatus(nil, requestID)
+	return result.Progress, err
+}
+
+// WithIPs allows for parameterizing the creation of an
+// Anonymization Job with a list of IP Addresses to
+// anonymize before invoking 'Create'.
 func (api *AnonymizationAPI) WithIPs(IPs []string) {
 	api.optionals.Ips = optional.NewInterface(IPs)
 }
 
-// WithUserIDs TODO: documentation
+// WithUserIDs allows for parameterizing the creation of an
+// Anonymization Job with a list of User IDs to
+// anonymize before invoking 'Create'.
 func (api *AnonymizationAPI) WithUserIDs(userIDs []string) {
 	api.optionals.UserIds = optional.NewInterface(userIDs)
 }
 
-// WithEndTimeStamp TODO: documentation
+// WithEndTimeStamp allows for parameterizing the creation of an
+// Anonymization Job with an end timestamp before invoking 'Create'.
+// Parameters:
+//	- timeStamp	is the end timestamp of the user session to anonymize,
+//				in UTC milliseconds.
+//				If 0, the current time is used.
 func (api *AnonymizationAPI) WithEndTimeStamp(timeStamp int64) {
 	api.optionals.EndTimestamp = optional.NewInt64(timeStamp)
 }
 
-// WithStartTimeStamp TODO: documentation
+// WithStartTimeStamp allows for parameterizing the creation of an
+// Anonymization Job with a start timestamp before invoking 'Create'.
+// Parameters:
+//	- timeStamp	is the start timestamp of the user session to anonymize,
+//				in UTC milliseconds.
+//				If 0, the earliest available time is used.
 func (api *AnonymizationAPI) WithStartTimeStamp(timeStamp int64) {
 	api.optionals.StartTimestamp = optional.NewInt64(timeStamp)
-}
-
-// CREATE TODO: documentation
-func (api *AnonymizationAPI) CREATE() (string, error) {
-	result, _, err := api.client.AnonymizationApi.Anonymize(nil, &api.optionals)
-	return result.RequestId, err
-}
-
-/*
-STATUS Shows the progress of the specified anonymization job.
- * @param requestID the unique identifier of the anonymization job
-@return int32 The progress of the anonymization job, percents. -1 if the job is waiting for execution
-*/
-func (api *AnonymizationAPI) STATUS(requestID string) (int32, error) {
-	result, _, err := api.client.AnonymizationApi.GetStatus(nil, requestID)
-	return result.Progress, err
 }
